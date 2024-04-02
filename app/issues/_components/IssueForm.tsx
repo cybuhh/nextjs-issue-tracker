a95bcf6dbd7a017ead1334429806c32952ede7c0
cybuhh <cybuhh@users.noreply.github.com>
2024-04-02 20:18:10 +0200

    improve loading form experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,19 +1,17 @@
 'use client';
 
 import { ErrorMessage, Spinner } from '@/app/components';
+import Editor from '@/app/components/EditorController';
 import { issueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Issue } from '@prisma/client';
 import { Button, Callout, TextField } from '@radix-ui/themes';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 type IssueFormData = z.infer<typeof issueSchema>;
-
-const Editor = dynamic(() => import('@/app/components/EditorController'), { ssr: false });
 
 interface IssueFormProps {
   issue?: Issue;
