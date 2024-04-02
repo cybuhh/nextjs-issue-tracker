a870f4f8e726175c6a72397ba0d4c037b04c724c
cybuhh <cybuhh@users.noreply.github.com>
2024-04-02 17:28:32 +0200

    refactor - apply Single Responsibility Principle

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+import { BsPencilSquare } from 'react-icons/bs';
+
+interface EditIssueButtonProps {
+  issueId: number;
+}
+
+function EditIssueButton({ issueId }: EditIssueButtonProps) {
+  return (
+    <Button>
+      <BsPencilSquare />
+      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+    </Button>
+  );
+}
+
+export default EditIssueButton;
