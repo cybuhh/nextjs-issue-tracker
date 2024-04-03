7900a60c67a6489ba307fe3fe9836ba6719362f5
cybuhh <cybuhh@users.noreply.github.com>
2024-04-03 22:16:38 +0200

    add issue summary component

---
+++ app/IssuesSummary.tsx
@@ -0,0 +1,37 @@
+import { Status } from '@prisma/client';
+import { Card, Flex, Text } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+interface IssuesSummaryProps {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+function IssuesSummary({ open, inProgress, closed }: IssuesSummaryProps) {
+  const containers: ReadonlyArray<{ label: string; value: number; status: Status }> = [
+    { label: 'Open issues', value: open, status: 'OPEN' },
+    { label: 'In-progress issues', value: inProgress, status: 'IN_PROGRESS' },
+    { label: 'Closed issues', value: closed, status: 'CLOSED' },
+  ];
+
+  return (
+    <Flex gap='4'>
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction='column' gap='1'>
+            <Link href={`/issues/list?status=${container.status}`} className='text-sm font-medium'>
+              {container.label}
+            </Link>
+            <Text size='5' className='font-bold'>
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+}
+
+export default IssuesSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import prisma from '@/prisma/client';
+import IssuesSummary from './IssuesSummary';
 import LatestIssues from './LatestIssues';
+import { Flex } from '@radix-ui/themes';
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+
+  return (
+    <Flex gap='3' direction='column'>
+      <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues />
+    </Flex>
+  );
 }
