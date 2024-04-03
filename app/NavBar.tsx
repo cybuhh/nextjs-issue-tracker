0d480d476b3daee4d82970a1d582232457d32f08
cybuhh <cybuhh@users.noreply.github.com>
2024-04-03 16:05:29 +0200

    fix avatar not loading because of referrer policy

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -45,7 +45,14 @@ function NavBar() {
             {status === 'authenticated' && (
               <DropdownMenu.Root>
                 <DropdownMenu.Trigger>
-                  <Avatar src={session.user!.image!} fallback='?' size='2' radius='full' className='cursor-pointer' />
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback='?'
+                    size='2'
+                    radius='full'
+                    className='cursor-pointer'
+                    referrerPolicy='no-referrer'
+                  />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
                   <DropdownMenu.Label>
