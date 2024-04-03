a1f90e267753d5b1925aa5448709a5bb15572a42
cybuhh <cybuhh@users.noreply.github.com>
2024-04-03 21:32:01 +0200

    implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useSearchParams, useRouter } from 'next/navigation';
 import React from 'react';
 import {
   MdKeyboardDoubleArrowLeft,
@@ -14,24 +17,38 @@ interface PaginationProps {
 }
 
 function Pagination({ itemCount, pageSize, currentPage }: PaginationProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
+  const onChangePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  };
 
   return (
     <Flex align='center' gap='2'>
       <Text size='2'>
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color='gray' variant='soft' disabled={currentPage === 1}>
+      <Button color='gray' variant='soft' disabled={currentPage === 1} onClick={() => onChangePage(1)}>
         <MdKeyboardDoubleArrowLeft />
       </Button>
-      <Button color='gray' variant='soft' disabled={currentPage === 1}>
+      <Button color='gray' variant='soft' disabled={currentPage === 1} onClick={() => onChangePage(currentPage - 1)}>
         <MdKeyboardArrowLeft />
       </Button>
-      <Button color='gray' variant='soft' disabled={currentPage === pageCount}>
+      <Button
+        color='gray'
+        variant='soft'
+        disabled={currentPage === pageCount}
+        onClick={() => onChangePage(currentPage + 1)}
+      >
         <MdKeyboardArrowRight />
       </Button>
-      <Button color='gray' variant='soft' disabled={currentPage === pageCount}>
+      <Button color='gray' variant='soft' disabled={currentPage === pageCount} onClick={() => onChangePage(pageCount)}>
         <MdKeyboardDoubleArrowRight />
       </Button>
     </Flex>
