e5b11f30be31c0e164aa2af80e4943c6172cb354
cybuhh <cybuhh@users.noreply.github.com>
2024-04-03 11:10:24 +0200

    add spinner to delete button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Spinner } from '@/app/components';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -11,8 +12,10 @@ interface DeleteIssueButtonProps {
 function DeleteIssueButton({ issueId }: DeleteIssueButtonProps) {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const handleOnDeleteButtonClick = async () => {
+    setIsDeleting(true);
     try {
       const response = await fetch(`/api/issues/${issueId}`, { method: 'DELETE' });
       if (!response.ok) {
@@ -22,6 +25,8 @@ function DeleteIssueButton({ issueId }: DeleteIssueButtonProps) {
       router.refresh();
     } catch (error) {
       setError(true);
+    } finally {
+      setIsDeleting(false);
     }
   };
 
@@ -31,7 +36,9 @@ function DeleteIssueButton({ issueId }: DeleteIssueButtonProps) {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color='red'>Delete Issue</Button>
+          <Button color='red' disabled={isDeleting}>
+            Delete Issue {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
