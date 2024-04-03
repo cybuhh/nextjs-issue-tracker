c89e77920f0fb6b445c5787b1effa018657330c3
cybuhh <cybuhh@users.noreply.github.com>
2024-04-03 20:21:18 +0200

    add filter component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,12 +1,16 @@
-import { Button } from '@radix-ui/themes';
+import { Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
 import React from 'react';
+import IssueStatusFilter from './IssueStatusFilter';
 
 function IssueActions() {
   return (
-    <Button>
-      <Link href='/issues/new'>New issue</Link>
-    </Button>
+    <Flex mb='5' justify='between'>
+      <IssueStatusFilter />
+      <Button>
+        <Link href='/issues/new'>New issue</Link>
+      </Button>
+    </Flex>
   );
 }
 
