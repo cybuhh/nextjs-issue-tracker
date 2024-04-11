c52e9b6464afd8a9c3d17b58aa7ae188d543c6ed
cybuhh <cybuhh@users.noreply.github.com>
2024-04-11 08:57:04 +0200

    fix typo

--- app/components/ErrorMessage.cy.tsx
+++ app/components/ErrorMessage.cy.tsx
@@ -2,7 +2,7 @@ import ErrorMessage from './ErrorMessage';
 
 describe('<ErrorMessage />', () => {
   describe('render and display expected content', () => {
-    it('should return null if not message provided', () => {
+    it('should return null if no message provided', () => {
       cy.mount(<ErrorMessage>null</ErrorMessage>);
       cy.getByTestId('data-testid').should('not.exist');
     });
