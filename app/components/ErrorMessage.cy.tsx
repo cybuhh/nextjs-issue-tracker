d6c41352bc06393d855bdbf88bfe417db6fae64a
cybuhh <cybuhh@users.noreply.github.com>
2024-04-12 18:38:42 +0200

    add Error component test (#13)
    * add error component test
    * update cypress run script

--- app/components/ErrorMessage.cy.tsx
+++ app/components/ErrorMessage.cy.tsx
@@ -3,8 +3,14 @@ import ErrorMessage from './ErrorMessage';
 describe('<ErrorMessage />', () => {
   describe('render and display expected content', () => {
     it('should return null if no message provided', () => {
-      cy.mount(<ErrorMessage>null</ErrorMessage>);
-      cy.getByTestId('data-testid').should('not.exist');
+      cy.mount(<ErrorMessage>{undefined}</ErrorMessage>);
+      cy.getByTestId('error-message').should('not.exist');
+    });
+
+    it('should show error message', () => {
+      const errorMesaage = 'Critical error has occurred';
+      cy.mount(<ErrorMessage>{errorMesaage}</ErrorMessage>);
+      cy.getByTestId('error-message').should('have.text', errorMesaage);
     });
   });
 });
